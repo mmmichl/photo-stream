34d3d8fce1362fa3bf53760708ec610a325f4ed0
mmmichl <mmmichlPi@gmail.com>
2018-08-09 12:54:00 +0200

    Add toolbar and button for file upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  flex: {
+    flexGrow: 1,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  hidden: {
+    display: 'none',
+  }
+});
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit" className={this.props.classes.flex}>
+            Annas <span role="img" aria-label="Brautpaar">👰 ⚭ 🤵</span>
+          </Typography>
+          <input
+            accept="image/*"
+            className={this.props.classes.hidden}
+            id="pic-upload-input"
+            type="file"
+            onChange={this.fileUpload}
+          />
+          <label htmlFor="pic-upload-input">
+            <Button component="span" variant="outlined" className={this.props.classes.button}>
+              Neues Foto
+            </Button>
+          </label>
+        </Toolbar>
+      </AppBar>
     );
+  }
+
+  fileUpload(evt) {
+    console.log('got file to upload', evt.currentTarget.files[0]);
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
